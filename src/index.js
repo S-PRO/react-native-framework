f3a96d3eacaced2622303aab6e41a3f32f906f43
galikvalkin <vgalkin@s-pro.io>
2016-03-28 16:40:50 +0300

    added structure

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import Button from './Button'
+import Footer from './Footer'
+import Header from './Header'
+import ImageSlider from './ImageSlider'
+import Select from './Select'
+import TabBar from './ImageSlider'
+
+export{
+    Button,
+    Footer,
+    Header,
+    ImageSlider,
+    Select,
+    TabBar
+}
