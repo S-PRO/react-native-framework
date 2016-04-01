24dde49a9bbe7b8f8eea17cc030de16e04e8fcc3
galikvalkin <vgalkin@s-pro.io>
2016-04-01 14:31:03 +0300

    added notification box component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,65 +1,84 @@
 'use strict';
 
-import React, { Component, View, StyleSheet, Text, TextInput } from 'react-native';
+import React, { Component, View, StyleSheet, Text, TextInput, Switch } from 'react-native';
+
+const styles = StyleSheet.create({
+    form:{
+        flex:1,
+        padding:10,
+        borderColor: 'grey',
+        borderWidth: 1,
+        marginTop:200,
+        marginLeft:20,
+        marginRight:20
+    },
+    input:{
+        marginTop:10,
+        height:30,
+        borderWidth:1,
+        borderColor:'#ddd',
+        borderRadius:2,
+        padding:5
+    },
+    highlight:{
+        borderColor:'green',
+        borderWidth:1
+    }
+});
 
 
+/**
+ * FormInput props:
+ * value, textChanged, styles, key
+ */
 class FormInput extends Component{
     constructor(props){
         super(props);
         this.state = {
-            value:''
-        };
+            highlight:false,
+            value:props.value
+        }
     }
-    valueChanged(text) {
-        this.setState({value: text});
+    focus(){
+        this.setState({highlight:true})
     }
-
+    blur(){
+        this.setState({highlight:false})
+    }
+    onTextChange(text){
+        this.setState({value:text});
+        this.props.textChanged(text);
+    }
     render(){
         return(
-            <TextInput  style={styles.input}
-                        onChangeText={this.valueChanged.bind(this)}
-                        keyboardType="default"
-                        value={this.state.value}/>
+            <TextInput
+                onBlur={this.blur.bind(this)}
+                onFocus={this.focus.bind(this)}
+                value={this.state.value}
+                onChangeText={this.onTextChange.bind(this)}
+                key={this.props.key}
+                style={[styles.input, this.props.styles, this.state.highlight ? styles.highlight : {}]}
+                keyboardType="default"/>
         )
+
     }
 }
-const styles = StyleSheet.create({
-    input:{
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius:10
-    }
-});
 
 class Form extends Component{
-    static propTypes = {
-        //validate:function(props, propName, componentName){
-            //if(!childrenIsValid(props)){
-            //    return new Error('Validation failed!');
-            //
-            //}
-        //}
-    };
     constructor(props){
         super(props);
-        console.log('props')
-        console.log(props)
-    }
-    onSubmit(){
 
     }
-
     render(){
         return(
-            <View style={{marginTop:200}}>
+            <View style={styles.form}>
                 {this.props.children}
             </View>
         )
     }
 }
-//<FormInput/>
 
 export {
-    Form, FormInput
+    Form,
+    FormInput
 }
