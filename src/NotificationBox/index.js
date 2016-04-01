97463522874fbfd4b05c4c12605833049f64cfba
galikvalkin <vgalkin@s-pro.io>
2016-04-01 16:55:01 +0300

    pull request changes

--- src/NotificationBox/index.js
+++ src/NotificationBox/index.js
@@ -4,15 +4,12 @@ import React, { Component, View, Text, StyleSheet } from 'react-native';
  * success, failure, message
  */
 class NotificationBox extends Component{
-    constructor(props){
-        super(props);
-    }
     render(){
         return(
         <View style={[styles.messageBox, this.props.success ? styles.successMessageBorder : styles.errorMessageBorder]}>
             <View style={[styles.leftBlock, this.props.success ? styles.successMessageLeftBlock : styles.errorMessageLeftBlock]}>
             </View>
-            <View style={[styles.rightBlock, this.props.success ? styles.successMessage : styles.errorMessage]}>
+            <View style={this.props.success ? styles.successMessage : styles.errorMessage}>
                 <Text>{this.props.message}</Text>
             </View>
         </View>
@@ -21,15 +18,12 @@ class NotificationBox extends Component{
 }
 
 class SuccessMessage extends Component{
-    constructor(props){
-        super(props);
-    }
     render(){
         return(
             <View style={[styles.messageBox, styles.successMessageBorder]}>
                 <View style={[styles.leftBlock, styles.successMessageLeftBlock]}>
                 </View>
-                <View style={[styles.rightBlock, styles.successMessage]}>
+                <View style={styles.successMessage}>
                     <Text>{this.props.message}</Text>
                 </View>
             </View>
@@ -38,15 +32,12 @@ class SuccessMessage extends Component{
 }
 
 class ErrorMessage extends Component{
-    constructor(props){
-        super(props);
-    }
     render(){
         return(
             <View style={[styles.messageBox, styles.errorMessageBorder]}>
                 <View style={[styles.leftBlock, styles.errorMessageLeftBlock]}>
                 </View>
-                <View style={[styles.rightBlock, styles.errorMessage]}>
+                <View style={styles.errorMessage}>
                     <Text>{this.props.message}</Text>
                 </View>
             </View>
@@ -63,7 +54,7 @@ class SystemMessage extends Component{
             <View style={[styles.messageBox, styles.systemMessageBorder]}>
                 <View style={[styles.leftBlock, styles.systemMessageLeftBlock]}>
                 </View>
-                <View style={[styles.rightBlock, styles.systemMessage]}>
+                <View style={styles.systemMessage}>
                     <Text>{this.props.message}</Text>
                 </View>
             </View>
@@ -119,12 +110,9 @@ const styles = StyleSheet.create({
         top:0,
         bottom:0,
         alignItems:'center',
-        padding:10,
-        flex:0.2
-    },
-    rightBlock:{
-        flex:4
+        padding:10
     }
+
 });
 
 export{
